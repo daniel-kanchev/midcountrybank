1344a418aa57d9f148a492f3f3cd5f05e812a1c5
daniel-kanchev <daniel.kanchev@adata.pro>
2021-04-01 14:18:59 +0300

    Finished Script

---
+++ README.md
@@ -0,0 +1,13 @@
+URL: https://www.midcountry.bank/about/news/
+
+    Spider name: midcountrybank
+
+DB Schema:
+- title
+- date
+- link
+- content
+
+Time: 11.68
+
+Number of articles: 38
---
+++ midcountrybank/settings.py
@@ -0,0 +1,13 @@
+BOT_NAME = 'midcountrybank'
+SPIDER_MODULES = ['midcountrybank.spiders']
+NEWSPIDER_MODULE = 'midcountrybank.spiders'
+USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
+ITEM_PIPELINES = {
+    'midcountrybank.pipelines.DatabasePipeline': 300,
+}
+FEED_EXPORT_ENCODING = 'utf-8'
+ROBOTSTXT_OBEY = True
+
+LOG_LEVEL = 'WARNING'
+
+# LOG_LEVEL = 'DEBUG'
